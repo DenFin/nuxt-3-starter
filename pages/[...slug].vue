09430814b981d8bfb430544ac7f8aef4b92e46b2
Dennis Fink <dennis.fink@kernpunkt.de>
2022-11-08 14:15:34 +0100

    feat: add tailwind and basic config

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,11 @@
 <template>
-  <main>Slug: {{ slug }}</main>
+    <main>Slug: {{ slug }}
+      <BegaButton text="Text" />
+  </main>
 </template>
 
 <script setup lang='ts'>
+import {BegaButton} from '@kernpunkt/bega-component-library'
 const route = useRoute()
 const slug = ref(route?.path)
 </script>
